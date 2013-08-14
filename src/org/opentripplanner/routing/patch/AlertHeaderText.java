3ff4e7eda3b13e244636f9c751d314c546083ec3
vreixo <vreixo@gmail.com>
2013-08-14 17:16:46 +0200

    Class missed implementation of serialization.

--- src/org/opentripplanner/routing/patch/AlertHeaderText.java
+++ src/org/opentripplanner/routing/patch/AlertHeaderText.java
@@ -1,10 +1,13 @@
 package org.opentripplanner.routing.patch;
 
+import java.io.Serializable;
+
 import lombok.Getter;
 import lombok.Setter;
 
-public class AlertHeaderText {
+public class AlertHeaderText implements Serializable{
 
+	private static final long serialVersionUID = -2726255462265642201L;
 	@Getter @Setter private Translations translations;
 	@Getter @Setter private String someTranslation;
 	
